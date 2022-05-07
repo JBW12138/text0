b3264fe52cde30a5fb0bcb8fcc564f8375a06186
JBW12138 <105102418+JBW12138@users.noreply.github.com>
2022-05-07 16:14:00 +0800

    Add files via upload

---
+++ test0.py
@@ -0,0 +1,43 @@
+# x=int(input('请输入一个数'))
+#
+# y=(x+10)*8/2//3%10
+# print('计算结果为',int(y**3))
+
+# a="A".encode()
+# for i in a:
+#     print(i)
+
+# l=[1,2,3]   #——列表
+# l.append(4)
+# print(l)
+
+# print(sorted('谁是我'))
+
+# d={"name":"Alex","age":18,"height":180}
+# d["weight"]=60
+# print(d)
+
+# lis=['1','2','3','4','5']
+# s='-'.join(lis)
+# print(lis)
+# print('-'.join(lis))
+# print(s.split('-',2))
+
+# s="大家好,mynameis:****"
+# s=s.split(':')
+# print(s[-1])
+# print(len(s[-1]))
+
+#  "123456789"  得到-》  "1,2,3,4,5,6,7,8,9"
+# s = "123456789"
+# s1 = ','.join(s)
+# print(s)
+# print(s1)
+
+# f = open("data.txt", "w+")
+# f.write("D")
+# f.write("ABC")
+# print(f.read())
+
+l=[1,2,3]
+print(sorted(l,reverse=True))
